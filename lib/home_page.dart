7c242cce64903f9c159886d12ddc9e65bbe67dd4
yeonkims <jiyeon7269@gmail.com>
2021-12-11 14:59:28 +0900

    Create test page

---
+++ lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("66 Days"),
+      ),
+      body: SizedBox.expand(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text("You clicked the button:"),
+            Text(
+              "$count",
+              style: Theme.of(context).textTheme.headline2?.apply(
+                    color: Theme.of(context).primaryColor,
+                  ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count += 1;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
